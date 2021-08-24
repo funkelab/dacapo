34cb1a222772e2bd274253a25c0d19d852aa7312
David Ackerman <ackermand@janelia.hhmi.org>
2021-08-24 14:27:03 -0400

    Update plotting to allow for selection of score names and other options

--- dacapo/plot.py
+++ dacapo/plot.py
@@ -1,3 +1,5 @@
+import json
+from bokeh.embed.standalone import json_item
 from dacapo.store.create_store import create_config_store, create_stats_store
 from bokeh.palettes import Category20 as palette
 import bokeh.layouts
@@ -31,7 +33,10 @@ def smooth_values(a, n, stride=1):
     return m, s
 
 
-def get_runs_info(run_config_names):
+def get_runs_info(run_config_base_names,
+                  validation_score_names,
+                  higher_is_betters,
+                  plot_losses):
 
     config_store = create_config_store()
     stats_store = create_stats_store()
@@ -44,35 +49,43 @@ def get_runs_info(run_config_names):
                           "trainer",
                           "dataset",
                           "training_stats",
-                          "validation_scores"])
+                          "validation_scores",
+                          "validation_score_name",
+                          "higher_is_better",
+                          "plot_loss"])
 
-    for run_config_name in run_config_names:
-        run_config = config_store.retrieve_run_config(run_config_name)
-        run = RunInfo(run_config_name,
-                      run_config.task_config.name,
-                      run_config.architecture_config.name,
-                      run_config.trainer_config.name,
-                      run_config.dataset_config.name,
-                      stats_store.retrieve_training_stats(
-                          run_config_name),
-                      stats_store.retrieve_validation_scores(
-                          run_config_name)
-                      )
-        runs.append(run)
+    all_run_config_names = config_store.retrieve_run_config_names()
+    for run_config_name in all_run_config_names:
+        run_config_base_name = run_config_name.split(":")[0]
+        if run_config_base_name in run_config_base_names:
+            idx = run_config_base_names.index(run_config_base_name)
+            validation_score_name = validation_score_names[idx]
+            higher_is_better = higher_is_betters[idx]
+            plot_loss = plot_losses[idx]
+
+            run_config = config_store.retrieve_run_config(run_config_name)
+            run = RunInfo(run_config_name,
+                          run_config.task_config.name,
+                          run_config.architecture_config.name,
+                          run_config.trainer_config.name,
+                          run_config.dataset_config.name,
+                          stats_store.retrieve_training_stats(
+                              run_config_name),
+                          stats_store.retrieve_validation_scores(
+                              run_config_name),
+                          validation_score_name,
+                          higher_is_better,
+                          plot_loss
+                          )
+            runs.append(run)
 
     return runs
 
 
-def plot_runs(run_config_names, smooth=100, validation_score=None):
-    relation, validation_score = validation_score.split(":")
-    if relation == "min":
-        higher_is_better = False
-    elif relation == "max":
-        higher_is_better = True
-    else:
-        raise Exception(f"Don't know how to handle relation: {relation}")
+def plot_runs(run_config_base_names, smooth=100, validation_scores=None, higher_is_betters=None, plot_losses=None, return_json=False):
 
-    runs = get_runs_info(run_config_names)
+    runs = get_runs_info(run_config_base_names,
+                         validation_scores, higher_is_betters, plot_losses)
 
     colors = itertools.cycle(palette[20])
     loss_tooltips = [
@@ -91,26 +104,35 @@ def plot_runs(run_config_names, smooth=100, validation_score=None):
     )
     loss_figure.background_fill_color = "#efefef"
 
-    validation_score_names = []
-    for r in runs:
-        if r.validation_scores.validated_until() > 0:
-            validation_score_names += r.validation_scores.get_score_names()
-    validation_score_names = np.unique(validation_score_names)
+    if validation_scores:
+        validation_score_names = []
+        validation_postprocessor_parameter_names = []
+        for r in runs:
+            if r.validation_scores.validated_until() > 0:
+                validation_score_names += r.validation_scores.get_score_names()
+                validation_postprocessor_parameter_names += r.validation_scores.get_postprocessor_parameter_names()
 
-    validation_tooltips = [
-        ("run", "@run"),
-        ("task", "@task"),
-        ("architecture", "@architecture"),
-        ("trainer", "@trainer"),
-        ("dataset",  "@dataset"),
-    ] + [(name, "@" + name) for name in validation_score_names]
-    validation_figure = bokeh.plotting.figure(
-        tools="pan, wheel_zoom, reset, save, hover",
-        x_axis_label="iterations",
-        tooltips=validation_tooltips,
-        plot_width=2048,
-    )
-    validation_figure.background_fill_color = "#efefef"
+        validation_score_names = np.unique(validation_score_names)
+        validation_postprocessor_parameter_names = np.unique(
+            validation_postprocessor_parameter_names)
+
+        validation_tooltips = [
+            ("run", "@run"),
+            ("task", "@task"),
+            ("architecture", "@architecture"),
+            ("trainer", "@trainer"),
+            ("dataset",  "@dataset"),
+        ] + [(name, "@" + name) for name in validation_score_names] \
+            + [(name, "@" + name)
+                for name in validation_postprocessor_parameter_names]
+
+        validation_figure = bokeh.plotting.figure(
+            tools="pan, wheel_zoom, reset, save, hover",
+            x_axis_label="iterations",
+            tooltips=validation_tooltips,
+            plot_width=2048,
+        )
+        validation_figure.background_fill_color = "#efefef"
 
     summary_tooltips = [
         ("run", "@run"),
@@ -133,8 +155,10 @@ def plot_runs(run_config_names, smooth=100, validation_score=None):
     )
     summary_figure.background_fill_color = "#efefef"
 
-    for run, color in zip(runs, colors):
+    include_validation_figure = False
+    include_loss_figure = False
 
+    for run, color in zip(runs, colors):
         if run.training_stats.trained_until() > 0:
 
             name = run.name
@@ -144,64 +168,77 @@ def plot_runs(run_config_names, smooth=100, validation_score=None):
                           for stat in run.training_stats.iteration_stats]
             losses = [stat.loss
                       for stat in run.training_stats.iteration_stats]
-            x, _ = smooth_values(
-                iterations, smooth, stride=smooth)
-            y, s = smooth_values(losses,
-                                 smooth, stride=smooth)
-            source = bokeh.plotting.ColumnDataSource(
-                {
-                    "iteration": x,
-                    "loss": y,
-                    "task": [run.task] * len(x),
-                    "architecture": [run.architecture] * len(x),
-                    "trainer": [run.trainer] * len(x),
-                    "dataset": [run.dataset] * len(x),
-                    "run": [str(run)] * len(x),
-                }
-            )
-            loss_figure.line(
-                "iteration",
-                "loss",
-                legend_label=name,
-                source=source,
-                color=color,
-                alpha=0.7,
-            )
 
-            loss_figure.patch(
-                np.concatenate([x, x[::-1]]),
-                np.concatenate([y + 3 * s, (y - 3 * s)[::-1]]),
-                legend_label=name,
-                color=color,
-                alpha=0.3,
-            )
+            if run.plot_loss:
+                include_loss_figure = True
+                x, _ = smooth_values(
+                    iterations, smooth, stride=smooth)
+                y, s = smooth_values(losses,
+                                     smooth, stride=smooth)
+                source = bokeh.plotting.ColumnDataSource(
+                    {
+                        "iteration": x,
+                        "loss": y,
+                        "task": [run.task] * len(x),
+                        "architecture": [run.architecture] * len(x),
+                        "trainer": [run.trainer] * len(x),
+                        "dataset": [run.dataset] * len(x),
+                        "run": [name] * len(x),
+                    }
+                )
+                loss_figure.line(
+                    "iteration",
+                    "loss",
+                    legend_label=name,
+                    source=source,
+                    color=color,
+                    alpha=0.7,
+                )
 
-        if validation_score and run.validation_scores.validated_until() > 0:
+                loss_figure.patch(
+                    np.concatenate([x, x[::-1]]),
+                    np.concatenate([y + 3 * s, (y - 3 * s)[::-1]]),
+                    legend_label=name,
+                    color=color,
+                    alpha=0.3,
+                )
 
-            x = run.validation_scores.iterations
+        if run.validation_score_name and run.validation_scores.validated_until() > 0:
+            include_validation_figure = True
+            x = [score.iteration
+                 for score in run.validation_scores.iteration_scores]
             source_dict = {
                 "iteration": x,
-                "task": [run.task.name] * len(x),
+                "task": [run.task] * len(x),
                 "architecture": [run.architecture] * len(x),
                 "trainer": [run.trainer] * len(x),
-                "dataset": [run.dataset.name] * len(x),
-                "run": [str(run)] * len(x),
+                "dataset": [run.dataset] * len(x),
+                "run": [run.name] * len(x),
             }
             # TODO: get_best: higher_is_better is not true for all scores
-            validation_averages = run.validation_scores.get_best(
-                validation_score, higher_is_better=higher_is_better
+            validation_bests = run.validation_scores.get_best(
+                run.validation_score_name, higher_is_better=run.higher_is_better
+            )
+            best_validation_parameters = validation_bests[0]
+            best_validation_scores = validation_bests[1]
+
+            source_dict.update(
+                {
+                    name: np.array(best_validation_parameters[name])
+                    for name in run.validation_scores.get_postprocessor_parameter_names()
+                }
             )
             source_dict.update(
                 {
-                    name: np.array(validation_averages[name])
+                    name: np.array(best_validation_scores[name])
                     for name in run.validation_scores.get_score_names()
                 }
             )
             source = bokeh.plotting.ColumnDataSource(source_dict)
             validation_figure.line(
                 "iteration",
-                validation_score,
-                legend_label=name + " " + validation_score,
+                run.validation_score_name,
+                legend_label=name + " " + run.validation_score_name,
                 source=source,
                 color=color,
                 alpha=0.7,
@@ -209,42 +246,55 @@ def plot_runs(run_config_names, smooth=100, validation_score=None):
 
     # Styling
     # training
-    loss_figure.title.text_font_size = "25pt"
-    loss_figure.title.text = "Training"
-    loss_figure.title.align = "center"
+    figures = []
+    if include_loss_figure:
+        loss_figure.title.text_font_size = "25pt"
+        loss_figure.title.text = "Training"
+        loss_figure.title.align = "center"
 
-    loss_figure.legend.label_text_font_size = "16pt"
+        loss_figure.legend.label_text_font_size = "16pt"
 
-    loss_figure.xaxis.axis_label = "Iterations"
-    loss_figure.xaxis.axis_label_text_font_size = "20pt"
-    loss_figure.xaxis.major_label_text_font_size = "16pt"
-    loss_figure.xaxis.axis_label_text_font = "times"
-    loss_figure.xaxis.axis_label_text_color = "black"
+        loss_figure.xaxis.axis_label = "Iterations"
+        loss_figure.xaxis.axis_label_text_font_size = "20pt"
+        loss_figure.xaxis.major_label_text_font_size = "16pt"
+        loss_figure.xaxis.axis_label_text_font = "times"
+        loss_figure.xaxis.axis_label_text_color = "black"
 
-    loss_figure.yaxis.axis_label = "Loss"
-    loss_figure.yaxis.axis_label_text_font_size = "20pt"
-    loss_figure.yaxis.major_label_text_font_size = "16pt"
-    loss_figure.yaxis.axis_label_text_font = "times"
-    loss_figure.yaxis.axis_label_text_color = "black"
+        loss_figure.yaxis.axis_label = "Loss"
+        loss_figure.yaxis.axis_label_text_font_size = "20pt"
+        loss_figure.yaxis.major_label_text_font_size = "16pt"
+        loss_figure.yaxis.axis_label_text_font = "times"
+        loss_figure.yaxis.axis_label_text_color = "black"
+        loss_figure.sizing_mode = 'scale_width'
+        figures.append(loss_figure)
 
-    # validation
-    validation_figure.title.text_font_size = "25pt"
-    validation_figure.title.text = "Validation"
-    validation_figure.title.align = "center"
+    if include_validation_figure:
+        # validation
+        validation_figure.title.text_font_size = "25pt"
+        validation_figure.title.text = "Validation"
+        validation_figure.title.align = "center"
 
-    validation_figure.legend.label_text_font_size = "16pt"
+        validation_figure.legend.label_text_font_size = "16pt"
 
-    validation_figure.xaxis.axis_label = "Iterations"
-    validation_figure.xaxis.axis_label_text_font_size = "20pt"
-    validation_figure.xaxis.major_label_text_font_size = "16pt"
-    validation_figure.xaxis.axis_label_text_font = "times"
-    validation_figure.xaxis.axis_label_text_color = "black"
+        validation_figure.xaxis.axis_label = "Iterations"
+        validation_figure.xaxis.axis_label_text_font_size = "20pt"
+        validation_figure.xaxis.major_label_text_font_size = "16pt"
+        validation_figure.xaxis.axis_label_text_font = "times"
+        validation_figure.xaxis.axis_label_text_color = "black"
 
-    validation_figure.yaxis.axis_label = f"{validation_score.capitalize()}"
-    validation_figure.yaxis.axis_label_text_font_size = "20pt"
-    validation_figure.yaxis.major_label_text_font_size = "16pt"
-    validation_figure.yaxis.axis_label_text_font = "times"
-    validation_figure.yaxis.axis_label_text_color = "black"
+        validation_figure.yaxis.axis_label = "Validation Score"
+        validation_figure.yaxis.axis_label_text_font_size = "20pt"
+        validation_figure.yaxis.major_label_text_font_size = "16pt"
+        validation_figure.yaxis.axis_label_text_font = "times"
+        validation_figure.yaxis.axis_label_text_color = "black"
+        validation_figure.sizing_mode = 'scale_width'
+        figures.append(validation_figure)
 
-    bokeh.plotting.output_file("performance_plots.html")
-    bokeh.plotting.save(bokeh.layouts.column(loss_figure, validation_figure))
+    plot = bokeh.layouts.column(*figures)
+    plot.sizing_mode = 'scale_width'
+
+    if return_json:
+        return json.dumps(json_item(plot, "myplot"))
+    else:
+        bokeh.plotting.output_file("performance_plots.html")
+        bokeh.plotting.save(plot)

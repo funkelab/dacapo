9cad874a68096625ea9b52c55b040eabd925a700
David Ackerman <ackermand@janelia.hhmi.org>
2021-08-26 10:17:13 -0400

    Simplify if else statement in plot code

--- dacapo/plot.py
+++ dacapo/plot.py
@@ -69,8 +69,8 @@ def get_runs_info(run_config_base_names,
                           run_config.architecture_config.name,
                           run_config.trainer_config.name,
                           run_config.dataset_config.name,
-                          [stats_store.retrieve_training_stats(
-                              run_config_name) if plot_loss else None][0],
+                          stats_store.retrieve_training_stats(
+                              run_config_name) if plot_loss else None,
                           stats_store.retrieve_validation_scores(
                               run_config_name),
                           validation_score_name,

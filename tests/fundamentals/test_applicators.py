dab011fb11b20e5a5064c9b21d2f1b104c0acf20
pattonw <pattonw@hhmi.org>
2021-09-15 18:54:16 -0400

    add serialization/deserialization test

--- tests/fundamentals/test_applicators.py
+++ tests/fundamentals/test_applicators.py
@@ -1,6 +1,10 @@
 from ..fixtures.fundamentals.applicators import APPLICATORS
 
+from dacapo.store.converter import converter
+from dacapo.fundamentals.applicators import Applicators
+
 import pytest
+
 
 from pathlib import Path
 
@@ -8,6 +12,7 @@ from pathlib import Path
 @pytest.mark.parametrize("applicator", APPLICATORS)
 def test_applicators(applicator):
 
+    # Test that the applicator provides all the necessary information
     assert applicator.name is not None and isinstance(applicator.name, str)
     assert applicator.keep_model_predictions is not None and isinstance(
         applicator.keep_model_predictions, bool
@@ -18,3 +23,10 @@ def test_applicators(applicator):
 
     assert isinstance(applicator.out_dir(Path("test"), 2, "test_data"), Path)
     assert isinstance(applicator.out_container(Path("test"), 2, "test_data"), str)
+
+    # Test that the applicator is serializable/deserializable
+    # so that it works properly with the database
+    serialized = converter.unstructure(applicator)
+    assert serialized == converter.unstructure(
+        converter.structure(serialized, Applicators)
+    )

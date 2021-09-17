887a876e44034f44791bfc3d860b73515bcf7a41
pattonw <pattonw@hhmi.org>
2021-09-17 11:45:55 -0400

    fix a typo

--- dacapo/groupings/experiments/helpers/experiment.py
+++ dacapo/groupings/experiments/helpers/experiment.py
@@ -7,5 +7,5 @@ from abc import ABC, abstractmethod
 @attr.s
 class Experiment(ABC):
     name: Optional[str] = attr.ib(
-        metadata={"help_text": "Name of your validator for easy search and reuse"},
+        metadata={"help_text": "Name of your experiment for easy search and reuse"},
     )

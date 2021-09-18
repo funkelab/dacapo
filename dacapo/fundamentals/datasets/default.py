5d4a40336b62483c0ff290ad73430375b38e1022
pattonw <pattonw@hhmi.org>
2021-09-18 16:17:59 -0400

    Random location now handled by dataset

--- dacapo/fundamentals/datasets/default.py
+++ dacapo/fundamentals/datasets/default.py
@@ -2,6 +2,9 @@ from .helpers import Dataset
 from dacapo.fundamentals.arraysources import ArraySource
 from dacapo.categoricals.datakeys import ArrayKey
 
+from gunpowder.array import ArrayKey as GPArrayKey
+from gunpowder.nodes import MergeProvider, RandomLocation
+
 import attr
 
 from typing import Optional
@@ -29,3 +32,15 @@ class Default(Dataset):
             return self.mask is not None
         else:
             return False
+
+    def random_location_provider(self, raw: GPArrayKey, gt: GPArrayKey, mask: GPArrayKey = None):
+        raw_source = self.raw.node(raw)
+        gt_source = self.gt.node(gt)
+        if self.mask is not None:
+            mask_source = self.mask.node(mask)
+            pipeline = (raw_source, gt_source, mask_source) + MergeProvider()
+        else:
+            pipeline = (raw_source, gt_source) + MergeProvider()
+
+        pipeline += RandomLocation()
+        return pipeline

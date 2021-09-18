114d31ebb94c37752fbfb93c5c8b1627fbf04fd3
pattonw <pattonw@hhmi.org>
2021-09-18 16:22:01 -0400

    add abstract methods to experiment

--- dacapo/groupings/experiments/helpers/experiment.py
+++ dacapo/groupings/experiments/helpers/experiment.py
@@ -2,6 +2,7 @@ import attr
 
 from typing import Optional
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 
 @attr.s
@@ -9,3 +10,47 @@ class Experiment(ABC):
     name: Optional[str] = attr.ib(
         metadata={"help_text": "Name of your experiment for easy search and reuse"},
     )
+
+    @property
+    @abstractmethod
+    def root_dir(self) -> Path:
+        pass
+
+    @abstractmethod
+    def is_valid(self):
+        """
+        Check whether all components of this experiment should work together.
+        """
+        pass
+
+    @abstractmethod
+    def can_train(self):
+        """
+        Check whether this experiment has the necessary components and
+        compatibilities to train.
+        """
+        pass
+
+    @abstractmethod
+    def can_validate(self):
+        """
+        Check whether this experiment has the necessary components and
+        compatibilities to validate.
+        """
+        pass
+
+    @abstractmethod
+    def can_apply(self):
+        """
+        Check whether this experiment has the necessary components and
+        compatibilities to apply to new datasets.
+        """
+        pass
+
+    @abstractmethod
+    def can_post_process(self):
+        """
+        Check whether this experiment has the necessary components and
+        compatibilities to apply to post process.
+        """
+        pass

0b43e38335996610fa10a1e292f7066669d9cf29
pattonw <pattonw@hhmi.org>
2021-09-18 16:22:43 -0400

    implement abstract methods on default experiment

--- dacapo/groupings/experiments/default.py
+++ dacapo/groupings/experiments/default.py
@@ -1,3 +1,4 @@
+from dacapo.fundamentals.architectures.helpers import architecture
 from .helpers import Experiment
 from dacapo.fundamentals.datasplits import DataSplit
 from dacapo.fundamentals.architectures import Architecture
@@ -6,9 +7,14 @@ from dacapo.fundamentals.pipelines import Pipeline
 from dacapo.fundamentals.trainers import Trainer
 from dacapo.fundamentals.validators import Validator
 from dacapo.fundamentals.executers import Executer
-from dacapo.store import ConfigStore, StatsStore
+from dacapo.store import ConfigStore, StatsStore, weights_store
+from dacapo.categoricals.datakeys import ArrayKey
+from dacapo.groupings.runs import Run, Default as DefaultRun
 
 import attr
+
+from typing import Optional
+from pathlib import Path
 
 
 @attr.s
@@ -30,10 +36,59 @@ class Default(Experiment):
     )
     trainer: Trainer = attr.ib(metadata={"help_text": "Training details go here"})
     validator: Validator = attr.ib(metadata={"help_text": "Validation details go here"})
-    executer: Executer = attr.ib(metadata={"help_text": "How to run your computations"})
     config_store: ConfigStore = attr.ib(
         metadata={"help_text": "Where to store your configs"}
     )
     stats_store: StatsStore = attr.ib(
         metadata={"help_text": "Where to store your stats"}
     )
+
+    @property
+    def root_dir(self):
+        return Path(f"experiments/{self.name}")
+
+    def is_valid(self):
+        return True
+        return all[
+            self.can_train(),
+            self.can_validate(),
+            self.can_apply(),
+            self.can_post_process(),
+        ]
+
+    def can_train(self):
+        assert self.datasplit.train.provides(
+            ArrayKey.RAW
+        ), f"Datasplit does not provide raw training data"
+        assert self.datasplit.train.raw.provides == self.architecture.takes, (
+            f"Model expects input: {self.architecture.takes} but the raw data "
+            f"provides {self.datasplit.train.raw.provides}"
+        )
+        assert self.architecture.provides == self.output.takes, (
+            f"Model heads expect input: {self.output.takes}, "
+            f"but the backbone provides: {self.architecture.provides}"
+        )
+        self.output.can_train()
+
+    def can_validate(self):
+        raise NotImplementedError()
+
+    def can_apply(self):
+        raise NotImplementedError()
+
+    def can_post_process(self):
+        raise NotImplementedError()
+
+    def run(self, repitition: Optional[int] = None) -> Run:
+        return DefaultRun(
+            experiment_name=self.name,
+            repitition=repitition,
+            root_dir=self.root_dir / f"runs/{repitition}",
+            stats_store=self.stats_store,
+            trainer=self.trainer,
+            validator=self.validator,
+            pipeline=self.pipeline,
+            datasplit=self.datasplit,
+            architecture=self.architecture,
+            output=self.output,
+        )

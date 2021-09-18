a016f9d3ceb0ad65b0cf5bff173f55527cefee64
pattonw <pattonw@hhmi.org>
2021-09-18 16:20:16 -0400

    Add abstract methods to pipeline abc

--- dacapo/fundamentals/pipelines/helpers/pipeline.py
+++ dacapo/fundamentals/pipelines/helpers/pipeline.py
@@ -6,3 +6,19 @@ from abc import ABC, abstractmethod
 @attr.s
 class Pipeline(ABC):
     name: str = attr.ib()
+
+    @abstractmethod
+    def init_train_pipeline(self, dataset):
+        pass
+
+    @abstractmethod
+    def init_validation_pipeline(self):
+        pass
+
+    @abstractmethod
+    def training_step(self):
+        pass
+
+    @abstractmethod
+    def validation_step(self):
+        pass

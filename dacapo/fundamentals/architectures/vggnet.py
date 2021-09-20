a1ace8421bc6cda546de1a17e3a252bfc59b527a
pattonw <pattonw@hhmi.org>
2021-09-20 19:05:34 -0400

    Make vggnet fit in DaCapo
    DaCapo generally assumes we are working in space. Since the vggnet
    outputs a single classification for each patch, it throws away a lot of
    spatial info like number of dimensions etc.
    We add that back in here.

--- dacapo/fundamentals/architectures/vggnet.py
+++ dacapo/fundamentals/architectures/vggnet.py
@@ -2,7 +2,9 @@ from .helpers import Architecture
 
 from funlib.learn.torch.models import Vgg3D as VGGNetModule
 from funlib.geometry import Coordinate
+
 import attr
+import torch
 
 from typing import List, Optional
 from enum import Enum
@@ -15,6 +17,12 @@ class VGGNet(Architecture):
     fmaps_out: int = attr.ib(
         metadata={"help_text": "The number of featuremaps provided."}
     )
+    fmap_inc: List[int] = attr.ib(
+        metadata={
+            "help_text": "The factors by which to increment channels at each downsample"
+        }
+    )
+    n_convolutions: List[int] = attr.ib()
     downsample_factors: List[Coordinate] = attr.ib(
         metadata={
             "help_text": "The factor by which to downsample spatial dimensions along each axis."
@@ -35,9 +43,24 @@ class VGGNet(Architecture):
         return Coordinate((1,) * self.input_shape.dims)
 
     def module(self):
-        return VGGNetModule(
-            input_size=self.input_shape,
-            fmaps=self.fmaps_in,
-            output_classes=self.fmaps_out,
-            downsample_factors=self.downsample_factors,
+        return torch.nn.Sequential(
+            VGGNetModule(
+                input_size=self.input_shape,
+                fmap_inc=self.fmap_inc,
+                n_convolutions=self.n_convolutions,
+                fmaps=self.fmaps_in,
+                output_classes=self.fmaps_out,
+                downsample_factors=self.downsample_factors,
+                input_fmaps=self.fmaps_in,
+            ),
+            AddSpatial(*self.output_shape),
         )
+
+
+class AddSpatial(torch.nn.Module):
+    def __init__(self, *args):
+        super(AddSpatial, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.view(x.shape + self.shape)

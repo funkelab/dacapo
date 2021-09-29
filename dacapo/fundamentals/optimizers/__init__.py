bfdff8a176db125471885a85f624eb37d2ce4548
pattonw <pattonw@hhmi.org>
2021-09-29 18:53:27 -0400

    Add null optimizer

--- dacapo/fundamentals/optimizers/__init__.py
+++ dacapo/fundamentals/optimizers/__init__.py
@@ -2,3 +2,4 @@ from .helpers import Optimizer  # noqa
 from .optimizers import Optimizers  # noqa
 from .adam import Adam  # noqa
 from .radam import RAdam  # noqa
+from .null import NullOptimizer  # noqa

c4342ec4329e009903fb8e1dccedb97c452b2d75
pattonw <pattonw@hhmi.org>
2021-10-11 17:00:06 -0400

    update tests

--- tests/operations/test_train.py
+++ tests/operations/test_train.py
@@ -2,11 +2,12 @@ from ..fixtures.fundamentals.datasplits import MK_FUNCTIONS
 from ..fixtures.fundamentals.outputs import OUTPUTS
 from ..fixtures.fundamentals.architectures import ARCHITECTURES
 from ..fixtures.fundamentals.starts import STARTS
-from ..fixtures.fundamentals.trainers import TRAINERS
 from ..fixtures.fundamentals.dataproviders import DATAPROVIDERS
 from ..fixtures.fundamentals.optimizers import OPTIMIZERS
 
+
 from dacapo import Experiment, train
+from dacapo.fundamentals.trainers import DefaultTrainer
 from dacapo.fundamentals.validators import Null as NullValidator
 from dacapo.store.debug_config_store import DebugConfigStore
 from dacapo.store.debug_stats_store import DebugStatsStore
@@ -19,7 +20,6 @@ import pytest
 @pytest.mark.parametrize("architecture", ARCHITECTURES)
 @pytest.mark.parametrize("output", OUTPUTS)
 @pytest.mark.parametrize("optimizer", OPTIMIZERS)
-@pytest.mark.parametrize("trainer", TRAINERS)
 @pytest.mark.parametrize("dataprovider", DATAPROVIDERS)
 def test_train(
     tmp_path,
@@ -28,15 +28,15 @@ def test_train(
     architecture,
     output,
     optimizer,
-    trainer,
     dataprovider,
 ):
     # make the temporary datasets to use:
     datasplit = mkfunction(tmp_path)
 
     name = "test_train"
-
+    trainer = DefaultTrainer(name="test_train", num_iterations=100, batch_size=1)
     validator = NullValidator()
+    
     config_store = DebugConfigStore()
     stats_store = DebugStatsStore()
 
@@ -52,9 +52,9 @@ def test_train(
         config_store=config_store,
         stats_store=stats_store,
     )
-    valid = experiment.is_valid()
+    can_train = experiment.can_train()
 
-    if not valid:
+    if not can_train:
         with pytest.raises(ValueError):
             train(experiment)
     else:
--- tests/operations/test_validate.py
+++ tests/operations/test_validate.py
@@ -1,14 +1,13 @@
 from ..fixtures.fundamentals.datasplits import MK_FUNCTIONS
 from ..fixtures.fundamentals.outputs import OUTPUTS
 from ..fixtures.fundamentals.architectures import ARCHITECTURES
-from ..fixtures.fundamentals.trainers import TRAINERS
 from ..fixtures.fundamentals.dataproviders import DATAPROVIDERS
-from ..fixtures.fundamentals.validators import VALIDATORS
-from ..fixtures.fundamentals.evaluators import EVALUATORS
-from ..fixtures.fundamentals.postprocessors import POSTPROCESSORS
 
-from dacapo import Experiment, validate
-from dacapo.fundamentals.optimizers import NullOptimizer
+from dacapo import Experiment, train
+from dacapo.fundamentals.optimizers import Adam
+from dacapo.fundamentals.trainers import DefaultTrainer
+from dacapo.fundamentals.validators import DefaultValidator
+from dacapo.fundamentals.dataproviders import GunpowderTrain, GunpowderValidate
 from dacapo.store.debug_config_store import DebugConfigStore
 from dacapo.store.debug_stats_store import DebugStatsStore
 
@@ -18,39 +17,37 @@ import pytest
 @pytest.mark.parametrize("mkfunction", MK_FUNCTIONS)
 @pytest.mark.parametrize("architecture", ARCHITECTURES)
 @pytest.mark.parametrize("output", OUTPUTS)
-@pytest.mark.parametrize("trainer", TRAINERS)
-@pytest.mark.parametrize("validator", VALIDATORS)
-@pytest.mark.parametrize("evaluator", EVALUATORS)
-@pytest.mark.parametrize("post_processor", POSTPROCESSORS)
 @pytest.mark.parametrize("dataprovider", DATAPROVIDERS)
 def test_validate(
     tmp_path,
     mkfunction,
     architecture,
     output,
-    trainer,
-    validator,
-    evaluator,
-    post_processor,
     dataprovider,
 ):
     # make the temporary datasets to use:
     datasplit = mkfunction(tmp_path)
 
     name = "test_validate"
+    trainer = DefaultTrainer(name="test_validate", num_iterations=100, batch_size=1)
+    validator = DefaultValidator(name="test_validate", validation_interval=1)
 
     config_store = DebugConfigStore()
     stats_store = DebugStatsStore()
 
-    null_optimizer = NullOptimizer()
+    optimizer = Adam("validation_test")
+
+    train_provider = GunpowderTrain(name="simple_gp_train")
+    val_provider = GunpowderValidate(name="simple_gp_validate")
 
     experiment = Experiment(
         name=name,
         datasplit=datasplit,
         architecture=architecture,
         output=output,
-        optimizer=null_optimizer,
-        dataprovider=dataprovider,
+        optimizer=optimizer,
+        train_provider=train_provider,
+        val_provider=val_provider,
         trainer=trainer,
         validator=validator,
         config_store=config_store,
@@ -60,6 +57,6 @@ def test_validate(
 
     if not can_validate:
         with pytest.raises(ValueError):
-            validate(experiment, 0, 0)
+            train(experiment)
     else:
-        validate(experiment, 0, 0)
+        train(experiment)

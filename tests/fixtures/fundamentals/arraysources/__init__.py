addfb42d1bee8cd3b8edeff8fe314025c84694d1
pattonw <pattonw@hhmi.org>
2021-10-11 16:58:51 -0400

    add validation data fixtures

--- tests/fixtures/fundamentals/arraysources/__init__.py
+++ tests/fixtures/fundamentals/arraysources/__init__.py
@@ -1,5 +1,5 @@
 from .bossdb import bossdb_intensities, bossdb_labels
-from .zarr import zarr_source, mk_train_raw, mk_train_gt
+from .zarr import zarr_source, mk_train_raw, mk_train_gt, mk_val_raw, mk_val_gt
 
 ARRAYSOURCES = [bossdb_labels, bossdb_intensities, zarr_source]
-MK_FUNCTIONS = [mk_train_raw, mk_train_gt]
+MK_FUNCTIONS = [mk_train_raw, mk_train_gt, mk_val_raw, mk_val_gt]

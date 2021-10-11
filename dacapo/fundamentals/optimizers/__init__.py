d36db4cbc3d29e23eae436e8cd7b7497d019d1f2
pattonw <pattonw@hhmi.org>
2021-10-11 16:58:22 -0400

    remove null optimizer
    its not that helpful for testing

--- dacapo/fundamentals/optimizers/__init__.py
+++ dacapo/fundamentals/optimizers/__init__.py
@@ -2,4 +2,3 @@ from .helpers import Optimizer  # noqa
 from .optimizers import Optimizers  # noqa
 from .adam import Adam  # noqa
 from .radam import RAdam  # noqa
-from .null import NullOptimizer  # noqa

a73754970731942067adfa7c25d56ddc5d207e47
pattonw <pattonw@hhmi.org>
2021-10-13 17:17:15 -0400

    update the architectures doc string

--- dacapo/fundamentals/architectures/helpers/architecture.py
+++ dacapo/fundamentals/architectures/helpers/architecture.py
@@ -10,8 +10,28 @@ class Architecture(ABC):
     The output of the Architecture will probably be some intermediate soft value
     that must be post processed to generate final results.
 
-    An Architecture in dacapo must have a name, and be able to produce a module
-    on demand.
+    Every architecture in DaCapo must define:
+    - `module` method:
+        A method that will produce a trainable torch module
+    - `input_shape` property:
+        The shape (excluding batch and channel dims) of the tensor that will
+        be passed into your module
+    - `output_shape` property:
+        The shape (excluding batch and channel dims) of the tensor that will
+        be returned from your module
+    - `fmaps_out` property:
+        The number of channels output from your module
+
+    Additionally it is encouraged to provide support for varying input/output shapes
+    during validation by overwriting the properties `eval_input_shape` and
+    `eval_output_shape`. By default they will be the same as the shape used
+    during training, however this can be extremely innefficient as you won't
+    use the full gpu memory.
+
+    Please note that abstract properties do not play well with attr.s classes
+    so to ensure that your Architecture fits the desired API please add an example
+    instance to the ARCHITECTURES fixture in
+    "tests/fixtures/stateless/architectures/__init__.py"
     """
     name: str = attr.ib(
         metadata={"help_text": "Name of your model for easy search and reuse"}

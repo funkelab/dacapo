ecd50272f6825ab9217c7dfa9347ec83db4b8cd5
pattonw <pattonw@hhmi.org>
2021-10-13 16:02:46 -0400

    update validation ABC to only take what is needed

--- dacapo/groupings/validations/helpers/validation.py
+++ dacapo/groupings/validations/helpers/validation.py
@@ -1,8 +1,6 @@
 from dacapo.store import StatsStore, WeightsStore
-from dacapo.fundamentals.trainers import Trainer
-from dacapo.fundamentals.validators import Validator
 from dacapo.fundamentals.dataproviders import DataProvider
-from dacapo.fundamentals.datasplits import DataSplit
+from dacapo.fundamentals.datasets import Dataset
 from dacapo.fundamentals.architectures import Architecture
 from dacapo.fundamentals.outputs import Output
 from dacapo.fundamentals.optimizers import Optimizer
@@ -19,17 +17,12 @@ class Validation(ABC):
     experiment_name: str = attr.ib(
         metadata={"help_text": "The experiment to which this run belongs"},
     )
-    repitition: int = attr.ib(
-        metadata={"help_text": "The repitition for your run"},
-    )
     root_dir: Path = attr.ib()
+    experiment_name: str = attr.ib()
+    repitition: int = attr.ib()
+    iteration: int = attr.ib()
     stats_store: StatsStore = attr.ib()
-
-    trainer: Trainer = attr.ib()
-    validator: Validator = attr.ib()
-    train_provider: DataProvider = attr.ib()
-    validation_provider: DataProvider = attr.ib()
-    datasplit: DataSplit = attr.ib()
+    checkpoint: Path = attr.ib()
+    dataset: Dataset = attr.ib()
     architecture: Architecture = attr.ib()
     output: Output = attr.ib()
-    optimizer: Optimizer = attr.ib()

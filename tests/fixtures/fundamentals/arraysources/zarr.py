7b749b8eae2cbf66c2a96db03aea3d2128be1018
pattonw <pattonw@hhmi.org>
2021-10-13 16:08:00 -0400

    update arraytypes in zarr array sources
    also shrink the validation dataset

--- tests/fixtures/fundamentals/arraysources/zarr.py
+++ tests/fixtures/fundamentals/arraysources/zarr.py
@@ -1,5 +1,5 @@
 from dacapo.fundamentals.arraysources import ZarrSource
-from dacapo.fundamentals.arraytypes import Intensities, Annotations
+from dacapo.basics.arraytypes import Intensities, Annotations
 
 import zarr
 import numpy as np
@@ -10,7 +10,7 @@ zarr_source = ZarrSource(
     name="test_zarr",
     filename=Path("test.zarr"),
     ds_name="volumes/raw",
-    array_type=Intensities(name="simple_intensities", shift=0, scale=0),
+    array_type=Intensities(shift=0, scale=0),
 )
 
 
@@ -46,7 +46,7 @@ def mk_train_raw(path):
         name="train_raw_zarr",
         filename=path / "data.zarr",
         ds_name="volumes/train/raw",
-        array_type=Intensities(name="simple_intensities", shift=0, scale=0),
+        array_type=Intensities(shift=0, scale=0),
     )
 
 
@@ -80,17 +80,19 @@ def mk_train_gt(path):
         name="train_gt_zarr",
         filename=path / "data.zarr",
         ds_name="volumes/train/gt",
-        array_type=Annotations(name="simple_annotations", num_classes=2),
+        array_type=Annotations(num_classes=2),
     )
 
+
 def mk_val_raw(path):
-    noise = np.random.randn(2, 100, 100, 100)
+    noise = np.random.randn(2, 50, 50, 50)
     label_0 = np.zeros_like(noise)
-    noise_0 = np.random.randn(2, 100, 100, 100)
+    noise_0 = np.random.randn(2, 50, 50, 50)
     label_1 = np.zeros_like(noise)
-    noise_1 = np.random.randn(2, 100, 100, 100)
-    label_0[0, 50:, :, :] = 1
-    label_1[1, :50, :, :] = 1
+    noise_1 = np.random.randn(2, 50, 50, 50)
+    for i in range(0, 50, 10):
+        label_0[i:i+5, :, :] = 1
+        label_1[i+5:i+10, :, :] = 1
     data = noise + label_0 * noise_0 * 0.6 + label_1 * noise_1 * 1.9
 
     container_path = path / "data.zarr"
@@ -100,7 +102,7 @@ def mk_val_raw(path):
         zarr_container = zarr.open(str(container_path.resolve()), mode="r+")
 
     dataset = zarr_container.create_dataset(
-        "volumes/val/raw", data=data, shape=[100, 100, 100], dtype=np.float64
+        "volumes/val/raw", data=data, shape=[50, 50, 50], dtype=np.float64
     )
     dataset.attrs["axes"] = ["c", "z", "y", "x"]
     dataset.attrs["resolution"] = (1, 1, 1)
@@ -111,16 +113,17 @@ def mk_val_raw(path):
         name="val_raw_zarr",
         filename=path / "data.zarr",
         ds_name="volumes/val/raw",
-        array_type=Intensities(name="simple_intensities", shift=0, scale=0),
+        array_type=Intensities(shift=0, scale=0),
     )
 
 
 def mk_val_gt(path):
-    noise = np.zeros((100, 100, 100), dtype=np.uint64)
+    noise = np.zeros((50, 50, 50), dtype=np.uint64)
     label_0 = np.zeros_like(noise)
     label_1 = np.zeros_like(noise)
-    label_0[50:, :, :] = 1
-    label_1[:50, :, :] = 1
+    for i in range(0, 50, 10):
+        label_0[i:i+5, :, :] = 1
+        label_1[i+5:i+10, :, :] = 1
     data = noise + label_1
 
     container_path = path / "data.zarr"
@@ -130,7 +133,7 @@ def mk_val_gt(path):
         zarr_container = zarr.open(str((path / "data.zarr").resolve()), mode="r+")
 
     dataset = zarr_container.create_dataset(
-        "volumes/val/gt", data=data, shape=[100, 100, 100], dtype=np.uint64
+        "volumes/val/gt", data=data, shape=[50, 50, 50], dtype=np.uint64
     )
     dataset.attrs["axes"] = ["z", "y", "x"]
     dataset.attrs["resolution"] = (1, 1, 1)
@@ -141,5 +144,5 @@ def mk_val_gt(path):
         name="val_gt_zarr",
         filename=path / "data.zarr",
         ds_name="volumes/val/gt",
-        array_type=Annotations(name="simple_annotations", num_classes=2),
+        array_type=Annotations(num_classes=2),
     )

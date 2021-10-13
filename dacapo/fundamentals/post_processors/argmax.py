cc67333578ea8ed12b298b8df5c66df093b12ce9
pattonw <pattonw@hhmi.org>
2021-10-13 15:55:08 -0400

    update the Argmax postprocessor

--- dacapo/fundamentals/post_processors/argmax.py
+++ dacapo/fundamentals/post_processors/argmax.py
@@ -1,6 +1,13 @@
 from .helpers import PostProcessor
+from dacapo.fundamentals.processing_steps import ArgMaxStep
+from dacapo.fundamentals.arraysources import ArraySource
+from dacapo.basics import Parameters
+
+import daisy
 
 import attr
+
+from typing import Tuple, List
 
 
 @attr.s
@@ -8,20 +15,16 @@ class ArgMax(PostProcessor):
     outputs: str = attr.ib(
         default="labels", metadata={"help_text": "The name of the provided outputs."}
     )
-    # steps: Tuple[ArgMaxStep] = attr.ib()
 
     def tasks(
         self,
         pred_id: str,
-        container,
-        input_dataset,
-        output_dataset,
-    ):
-        tasks, parameters = ArgMaxStep().tasks(
-            pred_id,
-            container,
-            input_dataset,
-            output_dataset,
+        in_source: ArraySource,
+        prediction_task: daisy.Task,
+    ) -> Tuple[daisy.Task, Parameters, ArraySource]:
+        parameters = Parameters(0)
+        tasks, parameter_sets, output_sources = ArgMaxStep("labels", None, 2).tasks(
+            pred_id, upstream_tasks=[[prediction_task], [parameters], [in_source]]
         )
 
-        return tasks, parameters
+        return tasks, parameter_sets, output_sources

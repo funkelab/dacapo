9032123d191c0efe542f174359b3fbd894caf4ff
pattonw <pattonw@hhmi.org>
2021-10-13 15:55:26 -0400

    remove predictors type

--- dacapo/fundamentals/predictors/__init__.py
+++ dacapo/fundamentals/predictors/__init__.py
@@ -1,5 +1,4 @@
 from .helpers import Predictor  # noqa
-from .predictors import Predictors # noqa
 from .affinities import Affinities  # noqa
 from .one_hot_labels import OneHotLabels  # noqa
 from .lsd import LSD  # noqa
--- dacapo/fundamentals/predictors/predictors.py
+++
@@ -1,7 +0,0 @@
-from .affinities import Affinities  # noqa
-from .one_hot_labels import OneHotLabels  # noqa
-from .lsd import LSD  # noqa
-
-from typing import Union
-
-Predictors = Union[Affinities, OneHotLabels, LSD]

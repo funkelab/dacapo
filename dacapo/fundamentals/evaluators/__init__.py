eadbb86875d0c7306bb12e6ffef5180de6e2b220
pattonw <pattonw@hhmi.org>
2021-10-13 15:51:52 -0400

    remove the Evaluators type
    The Evaluator subclass is sufficient for everything the type used to do

--- dacapo/fundamentals/evaluators/__init__.py
+++ dacapo/fundamentals/evaluators/__init__.py
@@ -1,5 +1,4 @@
 from .helpers import Evaluator  # noqa
-from .evaluators import Evaluators  # noqa
 from .affinity_evaluator import AffinityEvaluator  # noqa
 from .detection_evaluator import DetectionEvaluator  # noqa
 from .label_evaluator import LabelEvaluator  # noqa
--- dacapo/fundamentals/evaluators/evaluators.py
+++
@@ -1,7 +0,0 @@
-from .affinity_evaluator import AffinityEvaluator
-from .detection_evaluator import DetectionEvaluator
-from .label_evaluator import LabelEvaluator
-
-from typing import Union
-
-Evaluators = Union[AffinityEvaluator, DetectionEvaluator, LabelEvaluator]

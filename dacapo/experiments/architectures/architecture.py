91b12dbcf884c6ca1c173d485b54c32b0997f1fe
pattonw <pattonw@hhmi.org>
2021-10-21 12:50:30 -0400

    Add a scale factor to the architecture abc

--- dacapo/experiments/architectures/architecture.py
+++ dacapo/experiments/architectures/architecture.py
@@ -1,8 +1,10 @@
+from funlib.geometry import Coordinate
+
+from typing import Tuple
 from abc import ABC, abstractmethod
 
 
 class Architecture(ABC):
-
     @property
     @abstractmethod
     def input_shape(self):
@@ -26,3 +28,22 @@ class Architecture(ABC):
     def forward(self, x):
         """Process an input tensor."""
         pass
+
+    def scale_factor(self) -> Tuple[bool, Coordinate]:
+        """
+        The factor by which the input voxel size will differ from the output
+        voxel size.
+
+        Returns: `Tuple[up: bool, factor: Coordinate]`:
+            `up`: is a bool determining whether the output has a higher
+            resolution that the input.
+            `factor`: Determines the change in voxel size.
+
+        For example, if you input is 8x8x8 nm, and your output is 4x4x4 nm,
+        then you should return (true, Coordinate(2,2,2))
+        If your input is 4x4x4 nm, and your output is 12x12x12 nm,
+        then you should return (false, Coordinate(3,3,3))
+
+        Default behavior is to assume no change in scale.
+        """
+        return (True, Coordinate((1,)*self.input_shape.dims))
